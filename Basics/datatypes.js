a9eb762faafb29827854e99a4b4d59c16c96bcd8
Om2004-git <omdua2004@gmail.com>
2025-01-05 16:44:06 +0000

    changes done

---
+++ Basics/datatypes.js
@@ -0,0 +1,11 @@
+"use strict"; // treat all JS code as newer version 
+// datatypes
+// number => 2power53
+//bigint
+//string
+//boolean
+//null
+// object
+//symbol =>unique 
+console.log(typeof true);
+console.log(typeof null);
---
+++ Basics/var.js
@@ -0,0 +1,10 @@
+const accountId = 1231
+let accountEmail = "omdua@2004.gmil.com"
+var accountPassword = "1234"
+accountCity = " jaipur"
+accountEmail = "dhshd@"
+accountPassword = "212121" 
+console.table([accountId, accountEmail, accountPassword, accountCity])
+/*var is not used in todays because of issue in block scope and 
+functional scope 
+*/
