1a33d147d49ab9016e546312c7e3335066e22b2a
Om2004-git <omdua2004@gmail.com>
2025-01-06 07:44:17 +0000

    chnages

---
+++ Basics/Conversion_operations.js
@@ -0,0 +1,8 @@
+let string  = "33"
+//console.log(typeof string)
+let stringNumber = Number(string)
+//console.log(typeof stringNumber)
+let booleanNumber = 1 
+console.log(typeof booleanNumber)
+let covertBoolean = Boolean(booleanNumber)
+console.log(typeof covertBoolean)
